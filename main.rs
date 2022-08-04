e0c0d4d6f64edfbde744749ca5a3f23d5d0d4acc
Ipsa123 <110604656+Ipsa123@users.noreply.github.com>
2022-08-04 18:19:49 +0530

    Add files via upload

--- main.rs
+++ main.rs
@@ -1,8 +1,26 @@
-fn main() {
-  let mut s =String::from("aabbbdfsskklloopppwwzxcvbnm");
-  println!("{}",s);
+use std::io;
+fn main() 
+{
+println!("enter the first number");
+let mut num1 = String::new();
 
-  
+io::stdin()
+.read_line(&mut num1)
+.expect("failed to get input");
+
+
+println!("enter the second number");
+let mut num2 = String::new();
+
+io::stdin()
+.read_line(&mut num2)
+.expect("failed to get input");
+
+println!("The numbers are {} {}",num1,num2);
+
+let mut temp = num1;
+num1=num2;
+num2 = temp;
+println!("The swapped numbers are {} {}",num1,num2);
+    
 }
-
-
